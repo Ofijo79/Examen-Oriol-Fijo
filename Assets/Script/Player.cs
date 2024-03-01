675134300d4d8ff2a8aa428ced99306c20b6a652
Ofijo79 <oriolfijo2004@gmail.com>
2024-03-01 10:19:21 +0100

    Examen

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private UnityEngine.AI.NavMeshAgent _playerAgent;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _playerAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            SetDestination();
+        }
+    }
+
+    void SetDestination()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            _playerAgent.destination = hit.point;
+        }
     }
 }
