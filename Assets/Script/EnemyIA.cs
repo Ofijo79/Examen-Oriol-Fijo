2648aaf39223f97578c757e4118c0645593bdfa9
Ofijo79 <oriolfijo2004@gmail.com>
2024-03-01 10:04:20 +0100

    IA

--- Assets/Script/EnemyIA.cs
+++ Assets/Script/EnemyIA.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyIA : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
     enum State
     {
@@ -12,65 +12,115 @@ public class EnemyIA : MonoBehaviour
         Attacking
     }
 
-    [SerializeField] State currentState;
+    [SerializeField] private State currentState;
 
     NavMeshAgent agent;
 
-    [SerializeField] Transform[] points;
-
     Transform player;
 
-    [SerializeField] float visionRange = 15f;
+    [SerializeField] Transform[] patrolPoints;
 
-    [SerializeField] float attackRange = 5f;
-    
+    [SerializeField] float detectionRange = 15;
+
+    [SerializeField] float attackRange = 5;
+
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
-    // Start is called before the first frame update
+
+
     void Start()
     {
         SetPoint();
         currentState = State.Patrolling;
     }
 
-    // Update is called once per frame
     void Update()
     {
         switch(currentState)
         {
-            currentState State.Patrolling:
+            case State.Patrolling:
                 Patrol();
             break;
-            currentState State.Chasing:
+            case State.Chasing:
                 Chase();
             break;
-            currentState State.Attacking:
+            case State.Attacking:
                 Attack();
             break;
+        }
+    }
+
+    void Patrol()
+    {
+        if(IsInRange(detectionRange) == true)
+        {
+            currentState = State.Chasing;
+        }
+
+        if(agent.remainingDistance < 0.5f)
+        {
+            SetPoint();
+        }
+    }
+
+    void SetPoint()
+    {
+        agent.destination = patrolPoints[Random.Range(0, patrolPoints.Length)].position;
+    }
+
+    void Chase()
+    {
+        if(IsInRange(detectionRange) == false)
+        {
+            SetPoint();
+            currentState = State.Patrolling;
+        }
+
+        if(IsInRange(attackRange) == true)
+        {
+            currentState = State.Attacking;
+        }
+
+        agent.destination = player.position;
+    }
+
+    bool IsInRange(float range)
+    {
+        if(Vector3.Distance(transform.position, player.position) < range)
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
         }
     }
 
     void Attack()
     {
         Debug.Log("Atacando");
+
         currentState = State.Chasing;
-    }
-
-    void SetPoint()
-    {
-
-    }
-
-    void Chase()
-    {
-        
     }
 
     void DrawOnGizmos()
     {
-        
+        Gizmos.color = Color.blue;
+
+        foreach(Transform point in patrolPoints)
+        {
+            Gizmos.DrawWireSphere(point.position, 0.5f);
+        }
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
+
 }
